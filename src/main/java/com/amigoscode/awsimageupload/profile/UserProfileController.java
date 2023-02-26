4e82dbee16814ab787c790f2e3d1421ebf582a29
rafaelofficial <rafael.sousa.pereira@mail.usf.edu.br>
2023-02-26 17:56:24 -0300

    feat: implemented endpoint to do upload image from user profiles and check list to upload images logic

--- src/main/java/com/amigoscode/awsimageupload/profile/UserProfileController.java
+++ src/main/java/com/amigoscode/awsimageupload/profile/UserProfileController.java
@@ -1,11 +1,12 @@
 package com.amigoscode.awsimageupload.profile;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.UUID;
 
 @AllArgsConstructor
 @RestController
@@ -17,5 +18,13 @@ public class UserProfileController {
 		@GetMapping
 		public List<UserProfile> getUserProfiles() {
 				return userProfileService.getUserProfiles();
+		}
+
+		@PostMapping(path = "/{userProfileId}/image/upload",
+				consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+				produces = MediaType.APPLICATION_JSON_VALUE)
+		public void uploadUserProfileImage(@PathVariable("userProfileId") final UUID userProfileId,
+		                                   @RequestParam("file") final MultipartFile file) {
+				userProfileService.uploadUserProfileImage(userProfileId, file);
 		}
 }
--- src/main/java/com/amigoscode/awsimageupload/profile/UserProfileService.java
+++ src/main/java/com/amigoscode/awsimageupload/profile/UserProfileService.java
@@ -2,8 +2,10 @@ package com.amigoscode.awsimageupload.profile;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.UUID;
 
 @AllArgsConstructor
 @Service
@@ -15,4 +17,12 @@ public class UserProfileService {
 				return userProfileDataAccessService.getUserProfiles();
 		}
 
+		public void uploadUserProfileImage(final UUID userProfileId, final MultipartFile file) {
+				// TODO:
+				// 1. Check if image is not empty.
+				// 2. If file is an image.
+				// 3. The user exists in our database.
+				// 4. Grab some metadata from file is any.
+				// 5. Store the image in s3 and update database (userProfileImageLink) with s3 image link.
+		}
 }
