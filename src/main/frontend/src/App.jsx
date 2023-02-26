9115a3e9d24a939f416b3b4023c3d9a6ae8c7a4e
rafaelofficial <rafael.sousa.pereira@mail.usf.edu.br>
2023-02-26 19:42:35 -0300

    feat: implement dropzone to do upload of images

--- src/main/frontend/src/App.jsx
+++ src/main/frontend/src/App.jsx
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import './App.css';
 import axios from 'axios';
+import {useDropzone} from 'react-dropzone';
 
 const UserProfiles = () => {
 
@@ -20,12 +21,36 @@ const UserProfiles = () => {
   return userProfiles.map((userProfile, index) => {
     return (
       <div key={index}>
+        {/* todo: profile image */}
+        <br />
+        <br />
         <h1>{userProfile.userName}</h1>
         <p>{userProfile.userProfileId}</p>
+        <Dropzone />
+        <br />
       </div>
     );
   });
 };
+
+const Dropzone = () => {
+  const onDrop = useCallback(acceptedFiles => {
+    const file = acceptedFiles[0];
+    console.log(file);
+  }, [])
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
+  return (
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      {
+        isDragActive ?
+          <p>Drop the profile image here ...</p> :
+          <p>Drag 'n' drop profile image, or click to select profile image</p>
+      }
+    </div>
+  )
+}
 
 const App = () => {
 
